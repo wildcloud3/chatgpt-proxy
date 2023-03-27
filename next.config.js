3bcfe3425259d7dffc39d31fd4fa810e0431350b
Claud.Lee <wildcloud3@gmail.com>
2023-03-27 12:05:02 +0800

    Update next.config.js
    try extend timeout and add log for further debug

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverTimeout: 300000,
   async rewrites() {
+    const logMiddleware = (req, res, next) => {
+      console.log(`[${new Date().toISOString()}] ${req.url}`)
+      next()
+    }
+    
     return [
       {
         source: "/proxy/:slug*",
         destination: "https://api.openai.com/:slug*",
+        middleware: [logMiddleware],
       },
     ];
   },
