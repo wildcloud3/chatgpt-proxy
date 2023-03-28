80a1cb31d14e2c3c4df7d56c4beb5cac28ea0e55
Claud.Lee <wildcloud3@gmail.com>
2023-03-28 17:37:22 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  proxyTimeout: 120_000,
   async rewrites() {
-    const logMiddleware = (req, res, next) => {
-      console.log(`[${new Date().toISOString()}] ${req.url}`)
-      next()
-    }
-    
     return [
       {
         source: "/proxy/:slug*",
